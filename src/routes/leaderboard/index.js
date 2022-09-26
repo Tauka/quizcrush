2dac0a6aa78a2c15a64ecaeed5061dfe4ec079fa
Tauyekel Kunzhol <k.tauka@gmail.com>
2022-09-26 23:06:02 +0600

    move s3 inside function

--- src/routes/leaderboard/index.js
+++ src/routes/leaderboard/index.js
@@ -1,20 +1,20 @@
 import { S3 } from '@aws-sdk/client-s3';
 import { streamToString } from '../utils';
 
-if(process.env.NODE_ENV === 'development') {
-  const dotenv = await import('dotenv');
-  dotenv.default.config();
-}
-
-const s3 = new S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_ACCESS_SECRET
-  },
-  region: 'us-east-1'
-});
+//if(process.env.NODE_ENV === 'development') {
+  //const dotenv = await import('dotenv');
+  //dotenv.default.config();
+//}
 
 export async function get() {
+  const s3 = new S3({
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_ACCESS_SECRET
+    },
+    region: 'us-east-1'
+  });
+
   const response = await s3.getObject({
     Bucket: 'quizcrusher',
     Key: 'leaderboard.json'
